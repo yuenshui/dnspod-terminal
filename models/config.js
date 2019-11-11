e48044bd5122025c3913e77cd0180bda72a2ac71
于恩水 <yuenshui@126.com>
2019-11-11 09:46:40 +0800

    完善接口功能

--- models/config.js
+++ models/config.js
@@ -1,8 +1,8 @@
-'use strict';
-const debug = require("debug")("dnspod-terminal:lib:config");
+"use strict";
+const debug = require("debug")("dnspod-terminal:models:config");
 const fs = require("fs");
 const path = require("path");
-const dnspod = require("./dnspod");
+const dnspod = require("../lib/dnspod");
 const dir = path.dirname(__dirname) + "/config/";
 
 let configs = {};
@@ -13,41 +13,47 @@ module.exports.loadConfig = ctx => {
     .filter(file => /\.(json)$/i.test(file))
     .map(file => {
       try {
-        configs[file.replace(/\.json$/i, '')] = JSON.parse(fs.readFileSync(dir + file));
+        configs[file.replace(/\.json$/i, "")] = JSON.parse(
+          fs.readFileSync(dir + file)
+        );
       } catch (e) {
         debug("load config:", file, "error:", e.stack);
       }
     });
   if (ctx) {
-    ctx.body = configs;
-  }
-  else {
+    ctx.body = {
+      code: 1,
+      data: configs
+    };
+  } else {
     return configs;
   }
 };
 
 module.exports.get = id => {
-  if(Object.keys(configs).length == 0) this.loadConfig();
+  if (Object.keys(configs).length == 0) this.loadConfig();
   return configs[id] || null;
-}
+};
 
 module.exports.save = async id => {
-  if(!configs[id]) return 0;
+  if (!configs[id]) return 0;
   fs.writeFileSync(dir + id + ".json", JSON.stringify(configs[id], null, 2));
 };
 
 module.exports.addConfig = async ctx => {
   console.log(ctx.request.body);
-  if(!ctx.request.body || !ctx.request.body.id || !ctx.request.body.token) {
-    return ctx.body = {code: 403, msg: ""};
+  if (!ctx.request.body || !ctx.request.body.id || !ctx.request.body.token) {
+    return (ctx.body = { code: 403, msg: "" });
   }
   let id = ctx.request.body.id;
   let token = ctx.request.body.token;
   let client = await dnspod.init(ctx.request.body.id, ctx.request.body.token);
-  if(client.info && client.info.status) {
+  if (client.info && client.info.status) {
     configs[id] = {
-      id, token, info: client.info
-    }
+      id,
+      token,
+      info: client.info
+    };
     this.save(id);
     ctx.body = {
       code: 1,
@@ -59,4 +65,16 @@ module.exports.addConfig = async ctx => {
     };
   }
   return ctx.body;
-}
+};
+
+module.exports.remove = async ctx => {
+  if (!ctx.request.body || !ctx.request.body.id) {
+    return (ctx.body = { code: 403, msg: "" });
+  }
+  let id = parseInt(ctx.request.body.id);
+  if (configs[id]) delete configs[id];
+  fs.unlinkSync(`${dir}${id}.json`);
+  return (ctx.body = {
+    code: 1
+  });
+};
--- server/router.js
+++ server/router.js
@@ -1,8 +1,9 @@
-'use strict';
+"use strict";
 const debug = require("debug")("dnspod-terminal:router");
-const router = require('koa-router')();
+const router = require("koa-router")();
 const config = require("../lib/config");
 const domain = require("../lib/domain");
+const record = require("../lib/record");
 
 router.get("/configs", config.loadConfig);
 router.post("/configs", config.addConfig);
@@ -10,10 +11,12 @@ router.post("/configs", config.addConfig);
 router.get("/domains", domain.domains);
 router.get("/domain/purview", domain.domainPurview);
 router.post("/domain", domain.domainCreate);
+router.post("/domain/remark", domain.domainRemark);
+router.post("/domain/remove", domain.domainRemove);
 
-router.get("/records", domain.records);
-router.get("/record/types", domain.recordType);
-router.get("/record/line", domain.recordLine);
-router.post("/record", domain.recordCreate);
+router.get("/records", record.records);
+router.get("/record/types", record.recordType);
+router.get("/record/line", record.recordLine);
+router.post("/record", record.recordCreate);
 
 module.exports = router;
